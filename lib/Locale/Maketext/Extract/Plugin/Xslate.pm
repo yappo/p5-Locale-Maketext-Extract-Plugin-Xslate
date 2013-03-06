34e8ee9aa8c1588780820688d9ca8bc323b58aa4
Kazuhiro Osawa <yappo@shibuya.pl>
2013-03-06 17:28:20 +0900

    initial import

---
+++ lib/Locale/Maketext/Extract/Plugin/Xslate.pm
@@ -0,0 +1,41 @@
+package Locale::Maketext::Extract::Plugin::Xslate;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Locale::Maketext::Extract::Plugin::Xslate - ...
+
+=head1 SYNOPSIS
+
+  use Locale::Maketext::Extract::Plugin::Xslate;
+
+=head1 DESCRIPTION
+
+Locale::Maketext::Extract::Plugin::Xslate is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {@} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Osawa
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
